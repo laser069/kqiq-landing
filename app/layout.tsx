fd0bc320742db7a6263015b48572bafefbc005ab
selvadhanush <selva.dhanush.s81@kalvium.community>
2026-01-29 13:24:25 +0530

    contact

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/layout/Footer";
 import TextOverlayScroll from "@/components/ui/TextOverlayScroll";
 
 const schabo = localFont({
-  src: "../public/fonts/Oswald-Medium.ttf",
+  src: "../public/fonts/SCHABO-Condensed.otf",
   variable: "--font-schabo",
 });
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`light ${schabo.variable}`}>
       <body className="min-h-screen flex flex-col antialiased bg-white text-slate-900">
-        
+
         {/* Changed 'absolute' to 'fixed' to lock it to the top of the viewport */}
         <header className="fixed top-0 z-[100] w-full bg-transparent pointer-events-none">
           <div className="pointer-events-auto">
@@ -28,7 +28,7 @@ export default function RootLayout({
         <main className="flex-1 pt-0">
           {children}
         </main>
-        
+
         <Footer />
       </body>
     </html>
